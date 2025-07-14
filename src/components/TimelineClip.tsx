51610adb29dce133015f258cf32c7791be5f9974
HarryCMather <94319888+HarryCMather@users.noreply.github.com>
2025-07-14 16:29:36 +0100

    feat: Populate Timeline and Timeline Clip components

--- src/components/TimelineClip.tsx
+++ src/components/TimelineClip.tsx
@@ -1,8 +1,25 @@
 import "@muxit/css/TimelineClip.css";
+import { CSS } from "@dnd-kit/utilities";
+import { useSortable } from "@dnd-kit/sortable";
 
 function TimelineClip({ id, name }: { id: string; name: string }) {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
+
+  const style: React.CSSProperties = {
+    transform: CSS.Transform.toString(transform),
+    transition: transition
+  };
 
   return (
+    <div
+      ref={ setNodeRef }
+      className="timeline-item"
+      style={ style }
+      { ...attributes }
+      { ...listeners }
+    >
+      { name }
+    </div>
   );
 }
 
